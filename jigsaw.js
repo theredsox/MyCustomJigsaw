2dac51c550c9e105d3852ef907b246e8cae2466f
Brandon Williams <theredsox@gmail.com>
2024-06-20 06:21:27 -0400

    Implemented overlay cover, took a pass at better text readability (color contrast, font size and weight), zIndex strategy (main page 0-49, cover 50, overlays 100+), imeplemented a generic info overlay that any component can use, completed a first pass of the UI/UX for the play options screen. Need to change create puzzle page to an overlay and implement aspect ratio selection. After that finally ready to start coding the gameplay.

--- jigsaw.js
+++ jigsaw.js
@@ -1,9 +1,8 @@
 // Priority TODOs
 //
-// * In start game overlay, change the two dropdowns to better UI elements. Dropdowns aren't easy for mobile devices.
-// *    Consider using a solid background color for the overlay instead of the puzzle image
 // * Allow selection of ratio on 'create a puzzle' menu. Right now hardcoded to 3:2 for CROPPER.
-// * Add background "covers" to avoid  users interacting with background items when on an overlay (create puzzle, create folder, play menu)
+// * Change "page2", create puzzle, to the "overlay" format. No animation transition and use overlayCover. Page2 becomes board instead of page3
+// *    Consider how these overlays can be written more in HTML instead of JS. (create puzzle, create folder, play options)
 // * Create the game board and initialize the pieces
 // * Initial play functionality (snap pieces, rotate pieces, puzzle image for reference, zoom, buckets, sound on/off)
 // * Controls: left click+drag = move, right click = zoom on piece, left click+alt = rotate, ctrl+left click = multi-select pieces, shift+left click + drag = multi-select pieces in area
@@ -16,6 +15,9 @@
 // * Add handling of filesystem.js exceptions so that menu's continue to load if a file is missing and that an error is raised if a failure happens saving any of the images during puzzle creation
 // * BUG: Determine why fade out for pages works, but fade in happens fast. Maybe try non-CSS opacity fade, display none, display, opacity reveal
 // * Add loading message for puzzle creation (and maybe other areas like waiting on puzzle images)
+// * Simplify/clean up CSS. Like display: individual none/inline's using .remove and .add instead
+// * Consider using inline HTML format to create objects instead of separate lines for each attribute.
+//       Or find ways to reduce attribute setting. Maybe remove IDs that are identical to class name and look up object by class
 
 // Tracks the active folder for the puzzle menu
 var ACTIVE_FID = "root";
@@ -186,7 +188,6 @@ async function loadMenuItem(id, title) {
     var itemTitleSpan = window.document.createElement('span');
     itemTitleSpan.className = "menuItemTitle";
     itemTitleSpan.textContent = title;
-    itemTitleSpan.title = title;
     menuItem.appendChild(itemTitleSpan);
 
     // If the item is a folder, show the folder image. Otherwise show the custom puzzle portrait
@@ -254,6 +255,7 @@ function createFolder() {
         var name = document.getElementById("createFolderName").value;
         addFolder(fid, name);
 
+        hideOverlayCover();
         overlayDiv.remove();
 
         // TODO: Consider manually inserting the new DOM folder instead of reloading them all.
@@ -265,12 +267,18 @@ function createFolder() {
     // Cancel button to exit folder creation
     var cancelButton = window.document.createElement('button');
     cancelButton.innerText = "Cancel";
-    cancelButton.addEventListener("click", function(){ overlayDiv.remove(); });
+    cancelButton.addEventListener("click", function(){ 
+        hideOverlayCover();
+        overlayDiv.remove(); 
+    });
     overlayDiv.appendChild(cancelButton);
 
     // Add the div to the body to display it
     document.body.appendChild(overlayDiv);
-    
+
+    // Show the overlay cover
+    showOverlayCover();
+
     // Change focus to the name input
     input.focus();
 }
@@ -351,7 +359,9 @@ function loadPlayOverlay(menuItem, id, event) {
     // Clone the menuItem to use as the play overlay
     // Position it absolute right on top of its parent to start
     var playOverlay = menuItem.cloneNode(true);
-    playOverlay.id = "playOverlay";
+    playOverlay.id = "_" + playOverlay.id;  // Assure unique id
+    playOverlay.className = "playOverlay";
+    playOverlay.style.backgroundImage = menuItem.style.backgroundImage;
     playOverlay.style.position = "absolute";
     playOverlay.style.left = (event.pageX - event.offsetX) + "px";
     playOverlay.style.top = (event.pageY - event.offsetY) + "px";
@@ -361,38 +371,83 @@ function loadPlayOverlay(menuItem, id, event) {
     playOverlay.ondragend=null;
 
     // Select for difficulty - # of pieces 
-    let difficultyLabel = window.document.createElement('label');
-    difficultyLabel.className = "playOverlayLabel playOverlayHidden";
-    difficultyLabel.for = "playOverlayDifficulty";
-    difficultyLabel.innerText = "Choose your difficulty";
-
-    let difficultySelect = window.document.createElement('select');
-    difficultySelect.id = "playOverlayDifficulty";
-    difficultySelect.className = "playOverlayDifficulty";
+    let playOverlayDifficulties = window.document.createElement('div');
+    playOverlayDifficulties.className = "playOverlayDifficulties playOverlayHidden hidden";
     let puzzle = getPuzzle(ACTIVE_FID, id);
     let difficulties = getDifficulties(puzzle["aspectRatio"]);
     let index = 0;
     for (let [pieces, dimensions] of Object.entries(difficulties)) {
-        let difficulty = window.document.createElement('option');
-        difficulty.value = dimensions; // [width]x[height] as string
-        difficulty.innerText = pieces + " pieces - " + DIFFICULTIES[index];
-        difficultySelect.appendChild(difficulty);
+        let playOverlayDifficulty = window.document.createElement('div');
+        playOverlayDifficulty.id = dimensions;
+        playOverlayDifficulty.className = "playOverlayDifficulty";
+        playOverlayDifficulty.addEventListener("click", function() {
+            let selected = playOverlayDifficulties.querySelector(".playOverlayDifficultySelected");
+            if (selected && this != selected) {
+                selected.classList.remove("playOverlayDifficultySelected");
+            }
+            this.classList.add("playOverlayDifficultySelected");
+
+            document.getElementById("playButton").disabled = false;
+        });
+
+        // Play difficulty header
+        var difficultyHeaderDiv = window.document.createElement('div');
+        difficultyHeaderDiv.className = "difficultyHeader difficulty" + index;
+        playOverlayDifficulty.appendChild(difficultyHeaderDiv);
+
+        // Play difficulty title
+        var difficultyTitleSpan = window.document.createElement('span');
+        difficultyTitleSpan.className = "difficultyTitle";
+        difficultyTitleSpan.textContent = pieces + " pieces";
+        playOverlayDifficulty.appendChild(difficultyTitleSpan);
+
+        var difficultySpan = window.document.createElement('span');
+        difficultySpan.className = "difficultySpan";
+        difficultySpan.textContent = DIFFICULTIES[index];
+        playOverlayDifficulty.appendChild(difficultySpan);
+
+        playOverlayDifficulties.appendChild(playOverlayDifficulty);
         index++;
     }
-    difficultyLabel.appendChild(difficultySelect);
-    playOverlay.appendChild(difficultyLabel);
+    playOverlay.appendChild(playOverlayDifficulties);
 
-    // Select for Orientation of pieces
-    let orientationLabel = window.document.createElement('label');
-    orientationLabel.className = "playOverlayLabel playOverlayHidden";
-    orientationLabel.for = "playOverlayOrientation";
-    orientationLabel.innerText = "Puzzle piece orientation";
+    let playOverlayOrientation = window.document.createElement('div');
+    playOverlayOrientation.className = "playOverlayOrientation playOverlayHidden hidden";
 
+    // Play orientation header
+    var orientationHeaderDiv = window.document.createElement('div');
+    orientationHeaderDiv.className = "orientationHeader";
+    playOverlayOrientation.appendChild(orientationHeaderDiv);
+
+    // Play orientation title
+    var orientationTitleSpan = window.document.createElement('span');
+    orientationTitleSpan.className = "orientationTitle";
+    orientationTitleSpan.textContent = "Piece Orientation";
+
+    // Play info overlay describing piece orientation options
+    var orientationTitleInfoImg = window.document.createElement('img');
+    orientationTitleInfoImg.className = "orientationTitleInfoImg";
+    orientationTitleInfoImg.src = "assets/info-white.png";
+    orientationTitleInfoImg.title = "Click for detailed info on piece orientation options";
+    orientationTitleInfoImg.addEventListener("click", function() {
+        const msg = "Piece orientation refers to how the pieces will be laid out on the board." + 
+            "<ul>" + 
+            "<li><font class='orientationOption0'>In standard mode</font>, pieces will always be oriented in the correct direction. A flat edge at the top of the piece is a top edge piece.</li>" + 
+            "<li><font class='orientationOption1'>In intermediate mode</font>, pieces can be oriented two ways (north, south). A flat edge at the top of the piece could be a top or bottom edge piece. A flat edge on the left side is a left edge piece.</li>" + 
+            "<li><font class='orientationOption2'>In advanced mode</font>, pieces can be oriented in four ways (north, south, east, west). A flat edge at the top could be an edge for any side of the puzzle. This best represents physical jigsaw puzzles and will make the puzzle far more challenging.</li></ul>" +
+            "TIP: To rotate a piece, left click and hold to pick the piece up. Then hit the [Alt] key to rotate.";
+        document.getElementById("infoOverlayText").innerHTML = msg;
+        document.getElementById("infoOverlay").classList.remove("remove");
+    });
+    orientationTitleSpan.appendChild(orientationTitleInfoImg);
+    playOverlayOrientation.appendChild(orientationTitleSpan);
+
+    // Play orientation select
     let orientationSelect = window.document.createElement('select');
-    orientationSelect.id = "playOverlayOrientation";
-    orientationSelect.className = "playOverlayOrientation";
+    orientationSelect.className = "orientationSelect";
     for (let [index, label] of ORIENTATIONS.entries()) {
         let orientation = window.document.createElement('option');
+        orientation.className = "orientationOption" + index;
         orientation.value = index;
         orientation.innerText = label;
         orientation.selected = (index == 0);
@@ -400,58 +455,89 @@ function loadPlayOverlay(menuItem, id, event) {
     }
     orientationSelect.addEventListener("change", function() {
         var span = document.getElementById("playOverlayOrientationSpan");
-        span.style.display = (this.value != 0 ? "inline" : "none");
+        if (this.value != 0) {
+            span.classList.remove("playOverlayHidden");
+            span.classList.add("playOverlayVisible");
+        } else {
+            span.classList.add("playOverlayHidden");
+            span.classList.remove("playOverlayVisible");
+        }
+        this.className = "orientationSelect orientationOption" + this.value;
     });
-    orientationLabel.appendChild(orientationSelect);
-    playOverlay.appendChild(orientationLabel);
+    playOverlayOrientation.appendChild(orientationSelect);
 
     let orientationSpan = window.document.createElement('span');
     orientationSpan.id = "playOverlayOrientationSpan";
-    orientationSpan.className = "playOverlayLabel playOverlayOrientationSpan playOverlayHidden";
-    orientationSpan.innerText = "* NOTE: Press [Alt] key while holding a piece to rotate";
-    orientationSpan.style.display = "none";
-    playOverlay.appendChild(orientationSpan);
+    orientationSpan.className = "playOverlayOrientationSpan playOverlayHidden";
+    orientationSpan.innerText = "  * [Alt] key to rotate";
+    playOverlayOrientation.appendChild(orientationSpan);
+    
+    playOverlay.appendChild(playOverlayOrientation);
 
     // Play button
     var playButton = window.document.createElement('button');
     playButton.id = "playButton";
-    playButton.className = "playOverlayButton playOverlayHidden";
+    playButton.className = "playOverlayButton playOverlayHidden hidden";
+    playButton.disabled = true;
     playButton.innerText = "Play now";
     playButton.addEventListener("click", function(){ 
-        // TODO: Hide play overlay, hide page 1, and then show page 3 (the play board)
+        // Capture the user's play selections
+        const puzzleId = playOverlay.id.substring(1);
+        const difficulty = playOverlayDifficulties.querySelector(".playOverlayDifficultySelected").id;
+        const orientation = orientationSelect.value;
+
+        hideOverlayCover();
+        playOverlay.remove();
+
+        // Transition to the create puzzle page
+        displayPage("page1", false);
+        setTimeout(function(){
+            displayPage("page3", true);
+            startPuzzle(puzzleId, difficulty, orientation);
+        }, 600);
     });
     playOverlay.appendChild(playButton);
 
     // Cancel button to exit folder creation
     var cancelButton = window.document.createElement('button');
-    cancelButton.className = "playOverlayHidden";
+    cancelButton.className = "playOverlayHidden hidden";
     cancelButton.innerText = "Cancel";
-    cancelButton.addEventListener("click", function(){ playOverlay.remove(); });
+    cancelButton.addEventListener("click", function(){ 
+        hideOverlayCover();
+        playOverlay.remove();
+    });
     playOverlay.appendChild(cancelButton);
 
     document.body.appendChild(playOverlay);
 
     // Transition to the play overlay. Must be delayed for playOverlay to be drawn on screen or animations won't trigger.
     setTimeout(function() {
+        // Bring up the overlay cover
+        showOverlayCover();
+        
         // Apply the CSS class to trigger the transition position from menu item to play overlay
-        playOverlay.classList.add("playOverlay");
+        playOverlay.classList.add("playOverlayTransition");
 
         // Set background transparency to make play options more visible
         playOverlay.style.backgroundImage = "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), " + playOverlay.style.backgroundImage;
 
         // Make play options visible
         setTimeout(function() {
-            difficultyLabel.classList.add("playOverlayVisible");
-            difficultyLabel.classList.remove("playOverlayHidden");
-            orientationLabel.classList.add("playOverlayVisible");
-            orientationLabel.classList.remove("playOverlayHidden");
-            orientationSpan.classList.add("playOverlayVisible");
-            orientationSpan.classList.remove("playOverlayHidden");
+            // Bring them into the DOM before starting transitions
+            playOverlayDifficulties.classList.remove("hidden");
+            playOverlayOrientation.classList.remove("hidden");
+            playButton.classList.remove("hidden");
+            cancelButton.classList.remove("hidden");
+
+            playOverlayDifficulties.classList.add("playOverlayVisible");
+            playOverlayDifficulties.classList.remove("playOverlayHidden");
+            playOverlayOrientation.classList.add("playOverlayVisible");
+            playOverlayOrientation.classList.remove("playOverlayHidden");
             playButton.classList.add("playOverlayVisible");
             playButton.classList.remove("playOverlayHidden");
             cancelButton.classList.add("playOverlayVisible");
             cancelButton.classList.remove("playOverlayHidden");
-        }, 500);
+        }, 1000);
     }, 50);
 }
 
@@ -776,4 +862,24 @@ async function generatePuzzleImages(pid, dimensions) {
 function toggleDiskSpaceText() {
     var diskSpaceText = document.getElementById("diskSpaceText");
     diskSpaceText.style.display = diskSpaceText.style.display !== "inline" ? "inline" : "none";
+}
+
+function closeInfoOverlay() {
+    document.getElementById("infoOverlayText").innerHTML = "";
+    document.getElementById("infoOverlay").classList.add("remove");
+}
+
+function showOverlayCover() {
+    document.getElementById("overlayCover").classList.remove("remove");
+}
+
+function hideOverlayCover() {
+    document.getElementById("overlayCover").classList.add("remove");
+}
+
+// @param id - string - puzzle ID
+// @param difficulty - string - dimensions chosen - EX: "24x32"
+// @param orientation - integer - 0=no rotation, 1=north/south, 2=cardinal rotation
+function startPuzzle(id, difficulty, orientation) {
+    // TODO: prepare the board, build the pieces, spread them out on the board
 }
